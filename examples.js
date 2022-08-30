2f17bda2421dc6a1d21810753d36af807946648e
andanilov <andi.danilov@gmail.com>
2022-08-30 18:36:25 +0300

    readme

--- examples.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var validator_1 = require("./validator");
-console.log((0, validator_1["default"])(validator_1.rules.isRequired())(''));
-console.log((0, validator_1["default"])(validator_1.rules.isRequired('Обязательное поле для ввода!'), validator_1.rules.isEmail('Неверный формат записи Email!'))('32rf@ererer'));
-console.log((0, validator_1["default"])(validator_1.rules.isRequired('Обязательное поле для ввода!'), validator_1.rules.isLink('Неверный формат записи Ссылки!'))('http://qwqwqwqw.ru'));
